74132f75fae70dfa3464a73080a8374e59dcb392
DiyorbekAbdulhamidov <drkoffical001@gmail.com>
2023-03-31 23:23:53 +0500

    //

---
+++ main.js
@@ -0,0 +1,15 @@
+function sum() {
+    let numm = 0;
+    let nums = [1, 2, 3, 4, 5, "h"];
+  
+    nums.forEach(function(num) {
+      if (typeof num !== "number") {
+        throw new Error("ENVOLID TOKEN");
+      } else {
+        numm += num;
+      }
+    });
+  }
+  
+  sum();
+  
